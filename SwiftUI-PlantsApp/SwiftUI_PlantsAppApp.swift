af8ffe9a326033cea8b8d3e03f7258a44db97112
Arpit Dixit <arpitd@dewsolutions.in>
2021-09-01 10:23:33 +0530

    Initial Commit

---
+++ SwiftUI-PlantsApp/SwiftUI_PlantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlantsAppApp.swift
+//  SwiftUI-PlantsApp
+//
+//  Created by Arpit Dixit on 01/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
